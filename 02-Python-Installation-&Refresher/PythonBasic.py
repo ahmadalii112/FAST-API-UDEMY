7ac80280629a9913ad248c14cb87619144c3252d
Ahmad <ahmad.ali1@ibex.co>
2026-06-05 16:58:47 +0500

    Refresher

---
+++ 02-Python-Installation-&Refresher/PythonBasic.py
@@ -0,0 +1,35 @@
+print("Hello World")
+
+print(2 + 2)
+
+"""
+Variables
+"""
+
+costItem = 10
+taxPercentage = 0.25
+taxAmount = costItem * taxPercentage
+price = costItem + taxAmount
+print(price)
+
+username = "John Doe"
+first_name, last_name = username.split(" ")
+print(first_name)
+print(last_name)
+
+"""User Input"""
+
+# first_name = input("Please Enter Your Name:")
+print(f"Hello, {first_name}"
+      f"before you go, please take a break")
+
+"""Assignment"""
+
+
+days = int(input("How many days until their birthday?"))
+week = days // 7
+print(week)
+
+
+
+""" Lists are collections of items"""
