08d47a7b8518c5ad243a1695a8f41d75d19cfd5a
Ahmad <ahmad.ali1@ibex.co>
2026-07-27 18:37:14 +0500

    Data Validation Query Parameters

--- 06-Move-Fast-with-FastAPI/books.py
+++ 06-Move-Fast-with-FastAPI/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, Path
+from fastapi import FastAPI, Body, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -62,7 +62,7 @@ async def read_all_books():
 
 
 @app.get("/books/publish")
-async def read_books_by_publish_date(published_date: int):
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in Books:
         if book.published_date == published_date:
@@ -89,7 +89,7 @@ async def read_book(book_id: int = Path(gt=0)):
             return book
 
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
 
     books_to_return = []
     for book in Books:
